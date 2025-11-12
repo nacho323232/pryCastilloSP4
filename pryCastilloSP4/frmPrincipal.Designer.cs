03b40586367a654071f50efe4e3752d8e5da94bf
nacho323232 <castillojuanignacio@outlook.com>
2025-11-12 17:37:15 -0300

    Agregar archivos de proyecto.

---
+++ pryCastilloSP4/frmPrincipal.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace pryCastilloSP4
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDatos = new DataGridView();
+            colComida = new DataGridViewTextBoxColumn();
+            colBebidaNo = new DataGridViewTextBoxColumn();
+            colBebida = new DataGridViewTextBoxColumn();
+            colPostres = new DataGridViewTextBoxColumn();
+            btnValidarDatos = new Button();
+            btnMozo = new Button();
+            btnTotal = new Button();
+            btnCerrar = new Button();
+            lblMozo = new Label();
+            lblTotal = new Label();
+            lstTotales = new ListBox();
+            txtNombreMozo = new TextBox();
+            txtImporteMozo = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.AllowUserToAddRows = false;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Columns.AddRange(new DataGridViewColumn[] { colComida, colBebidaNo, colBebida, colPostres });
+            dgvDatos.Location = new Point(47, 21);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.Size = new Size(721, 203);
+            dgvDatos.TabIndex = 0;
+            // 
+            // colComida
+            // 
+            colComida.HeaderText = "Comida";
+            colComida.Name = "colComida";
+            // 
+            // colBebidaNo
+            // 
+            colBebidaNo.HeaderText = "Bebida sin alchol";
+            colBebidaNo.Name = "colBebidaNo";
+            // 
+            // colBebida
+            // 
+            colBebida.HeaderText = "Bebida Con Alcohol";
+            colBebida.Name = "colBebida";
+            // 
+            // colPostres
+            // 
+            colPostres.HeaderText = "Postres";
+            colPostres.Name = "colPostres";
+            // 
+            // btnValidarDatos
+            // 
+            btnValidarDatos.Location = new Point(54, 239);
+            btnValidarDatos.Name = "btnValidarDatos";
+            btnValidarDatos.Size = new Size(119, 23);
+            btnValidarDatos.TabIndex = 1;
+            btnValidarDatos.Text = "Validar Datos";
+            btnValidarDatos.UseVisualStyleBackColor = true;
+            btnValidarDatos.Click += btnValidarDatos_Click;
+            // 
+            // btnMozo
+            // 
+            btnMozo.Location = new Point(490, 374);
+            btnMozo.Name = "btnMozo";
+            btnMozo.Size = new Size(116, 23);
+            btnMozo.TabIndex = 2;
+            btnMozo.Text = "Mozo del dia";
+            btnMozo.UseVisualStyleBackColor = true;
+            btnMozo.Click += btnMozo_Click;
+            // 
+            // btnTotal
+            // 
+            btnTotal.Location = new Point(628, 374);
+            btnTotal.Name = "btnTotal";
+            btnTotal.Size = new Size(116, 23);
+            btnTotal.TabIndex = 3;
+            btnTotal.Text = "Totales";
+            btnTotal.UseVisualStyleBackColor = true;
+            btnTotal.Click += btnTotal_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(603, 403);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(116, 23);
+            btnCerrar.TabIndex = 4;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // lblMozo
+            // 
+            lblMozo.AutoSize = true;
+            lblMozo.Location = new Point(54, 308);
+            lblMozo.Name = "lblMozo";
+            lblMozo.Size = new Size(75, 15);
+            lblMozo.TabIndex = 5;
+            lblMozo.Text = "Mozo del dia";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Location = new Point(47, 374);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(76, 15);
+            lblTotal.TabIndex = 6;
+            lblTotal.Text = "Total General";
+            // 
+            // lstTotales
+            // 
+            lstTotales.FormattingEnabled = true;
+            lstTotales.ItemHeight = 15;
+            lstTotales.Location = new Point(148, 374);
+            lstTotales.Name = "lstTotales";
+            lstTotales.Size = new Size(312, 64);
+            lstTotales.TabIndex = 7;
+            lstTotales.SelectedIndexChanged += lstTotales_SelectedIndexChanged;
+            // 
+            // txtNombreMozo
+            // 
+            txtNombreMozo.Location = new Point(148, 305);
+            txtNombreMozo.Name = "txtNombreMozo";
+            txtNombreMozo.Size = new Size(213, 23);
+            txtNombreMozo.TabIndex = 8;
+            // 
+            // txtImporteMozo
+            // 
+            txtImporteMozo.Location = new Point(377, 305);
+            txtImporteMozo.Name = "txtImporteMozo";
+            txtImporteMozo.Size = new Size(193, 23);
+            txtImporteMozo.TabIndex = 9;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtImporteMozo);
+            Controls.Add(txtNombreMozo);
+            Controls.Add(lstTotales);
+            Controls.Add(lblTotal);
+            Controls.Add(lblMozo);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnTotal);
+            Controls.Add(btnMozo);
+            Controls.Add(btnValidarDatos);
+            Controls.Add(dgvDatos);
+            Name = "frmPrincipal";
+            Text = "frmPrincipal";
+            Load += frmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvDatos;
+        private Button btnValidarDatos;
+        private Button btnMozo;
+        private Button btnTotal;
+        private Button btnCerrar;
+        private Label lblMozo;
+        private Label lblTotal;
+        private ListBox lstTotales;
+        private TextBox txtNombreMozo;
+        private TextBox txtImporteMozo;
+        private DataGridViewTextBoxColumn colComida;
+        private DataGridViewTextBoxColumn colBebidaNo;
+        private DataGridViewTextBoxColumn colBebida;
+        private DataGridViewTextBoxColumn colPostres;
+    }
+}
---
+++ pryCastilloSP4/frmPrincipal.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCastilloSP4
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        //Declaración de Matriz
+        float[,] vecDatos = new float[5, 5];
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+            string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+            foreach (string mozo in mozos)
+            {
+                int index = dgvDatos.Rows.Add();
+                dgvDatos.Rows[index].HeaderCell.Value = mozo;
+            }
+
+            dgvDatos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDatos.RowHeadersWidth = 100;
+
+            dgvDatos.CurrentCell = dgvDatos.Rows[0].Cells[1];
+
+            for (int i = 0; i < dgvDatos.Rows.Count; i++)
+            {
+                for (int j = 0; j < dgvDatos.Columns.Count; j++)
+                {
+                    dgvDatos.Rows[i].Cells[j].Value = "0";
+                }
+            }
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+            bool datosValidos = true;
+            //recorrer la grilla
+            for (int i = 0; i < dgvDatos.Rows.Count; i++)
+            {
+                for (int j = 0; j < dgvDatos.Columns.Count; j++)
+                {
+                    object valor = dgvDatos.Rows[i].Cells[j].Value;
+                    //validar que no este vacio
+                    if (valor == null || string.IsNullOrWhiteSpace(valor.ToString()))
+                    {
+                        datosValidos = false;
+                        break;
+                    }
+                    //validar que sean numeros
+                    if (!float.TryParse(valor.ToString(), out _))
+                    {
+                        datosValidos = false;
+                        break;
+                    }
+                }
+                if (!datosValidos)
+                    break;
+            }
+
+            //muestro el resultado
+            if (!datosValidos)
+            {
+                MessageBox.Show("Error: todas las celdas deben contener valores numéricos (puede ser 0).",
+                                "Datos inválidos",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                btnMozo.Enabled = false;
+                btnTotal.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Datos validados correctamente.",
+                                "Éxito",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                btnMozo.Enabled = true;
+                btnTotal.Enabled = true;
+            }
+        }
+
+        private void btnMozo_Click(object sender, EventArgs e)
+        {
+            string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+            float[,] ventas = new float[5, 4];
+            //cargo los valores de la grilla en una matriz
+            // C# - opción simple: saltar filas "nuevas"
+            int rowCount = dgvDatos.Rows.Count - (dgvDatos.AllowUserToAddRows ? 1 : 0);
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < dgvDatos.Columns.Count; j++)
+                {
+                    if (dgvDatos.Rows[i].IsNewRow) continue;
+                    ventas[i, j] = float.Parse(dgvDatos.Rows[i].Cells[j].Value.ToString());
+                }
+            }
+            //calculo el total para cada mozo
+            float[] totalPorMozo = new float[5];
+            for (int i = 0; i < 5; i++)
+            {
+                float total = 0;
+                for (int j = 0; j < 4; j++)
+                    total += ventas[i, j];
+                totalPorMozo[i] = total;
+            }
+            //busco el numero maximo
+            float maxTotal = totalPorMozo.Max();
+            //busco los mozos con ese numero para ver si hay empate
+            var mozosEmpatados = new List<string>();
+            for (int i = 0; i < mozos.Length; i++)
+            {
+                if (totalPorMozo[i] == maxTotal)
+                    mozosEmpatados.Add(mozos[i]);
+            }
+            //muestro resultado
+            txtImporteMozo.Text = maxTotal.ToString("0.00");
+            if (mozosEmpatados.Count == 1)
+            {
+                txtNombreMozo.Text = mozosEmpatados[0];
+            }
+            else
+            {
+                txtNombreMozo.Text = string.Join(", ", mozosEmpatados);
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int rowCount = dgvDatos.Rows.Count - (dgvDatos.AllowUserToAddRows ? 1 : 0);
+            int colCount = dgvDatos.Columns.Count;
+
+            float[,] ventas = new float[rowCount, colCount];
+
+            // Cargo los valores de la grilla a la matriz (parse seguro)
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    var cell = dgvDatos.Rows[i].Cells[j].Value;
+                    if (!float.TryParse(cell?.ToString() ?? "0", out float v))
+                        v = 0f;
+                    ventas[i, j] = v;
+                }
+            }
+
+            // Calculo totales por categoría (columnas)
+            float[] totalesCategoria = new float[colCount];
+            float totalGeneral = 0f;
+            for (int j = 0; j < colCount; j++)
+            {
+                float suma = 0f;
+                for (int i = 0; i < rowCount; i++)
+                    suma += ventas[i, j];
+                totalesCategoria[j] = suma;
+                totalGeneral += suma;
+            }
+
+            // Mostrar resultado (usar los totales calculados)
+            lstTotales.Items.Clear();
+            lstTotales.Items.Add($"Comida: {totalesCategoria[0]:0.00}");
+            lstTotales.Items.Add($"Bebidas sin alcohol: {totalesCategoria[1]:0.00}");
+            lstTotales.Items.Add($"Bebidas con alcohol: {totalesCategoria[2]:0.00}");
+            lstTotales.Items.Add($"Postres: {totalesCategoria[3]:0.00}");
+            lstTotales.Items.Add($"TOTAL GENERAL: {totalGeneral:0.00}");
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lstTotales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
